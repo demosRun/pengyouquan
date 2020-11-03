a7110eecfb0fe1695bcb1e2b1a2eb05380d886bb
puge <mail@puge.org>
2020-11-03 19:54:10 +0800

    save

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -25,3 +25,31 @@ setTimeout(() => {
     }
   })
 }, 100);
+
+window.isWeiXin = /MicroMessenger/i.test(navigator.userAgent)
+var turnHref = 'http://paike.people.com.cn/lianliankan/Daliangshan/index'
+
+// 如果是微信则跳转到 授权页面
+if (window.isWeiXin) {
+  if (location.href.indexOf('id=') > -1) {
+    var arr = /id=([a-z0-9A-Z]+)/.exec(location.href)
+    if (arr) {
+      window.pid = arr[1]
+      console.log('是微信打开的，pid是' + window.pid)
+      if (arr[1]) {
+        $.get('http://paike.people.com.cn/lianliankan/Daliangshan/getUser', {id: window.pid}, function(data) {
+          alert(data)
+          // document.querySelector('.page5 .so-2').src = data.msg.headImg
+        }, 'jsonp');
+      }
+      
+      
+    }
+  } else {
+    if (/Android/i.test(navigator.userAgent)) {
+      location.href = turnHref + '?t=' + new Date()
+    } else {
+      location.href = turnHref
+    }
+  }
+}
